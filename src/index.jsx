51d54bacaf7c08b253ab10dd6ef9047a4e9435a1
why985123 <hongying.wang@xibao100.com>
2016-12-26 18:27:48 +0800

    .

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,9 @@
 'use strict';
 import {mount} from 'nuke-mounter';
 import {createElement, Component} from 'weex-rx';
-import {View, Text, Modal} from 'nuke';
+import {View, Text, Button, Modal} from 'nuke';
 import QN from 'QAP-SDK';
+
 
 class Demo extends Component {
     constructor(props) {
@@ -10,6 +11,20 @@ class Demo extends Component {
     
       this.state = {};
     }
+
+     handleTOPInvoke() {
+        QN.top.invoke({
+            query: {
+                method: 'taobao.time.get'
+            }
+        }).then((result) => {
+            Modal.toast(result.time_get_response.time);
+        }, (error) => {
+            Modal.toast(error);
+        });
+    }
+
+
     render() {
         return (
             <View style={styles.container}>
@@ -20,12 +35,14 @@ class Demo extends Component {
                     编辑src/index.jsx文件，开始QAP之旅，
                 </Text>
                 <Text style={styles.instructions}>
-                    点击手机千牛右上角刷新，
+                    点击手机千牛右上角刷新ok，
                 </Text>
                 <Text style={styles.instructions}>
-                    或者刷新浏览器立即查看效果
+                    或者刷新浏览器立即查看效果ddddd
                 </Text>
+                  <Button block="true" onPress={() => {this.handleTOPInvoke()}} type="primary">QN.top.invoke</Button>
             </View>
+           
         );
     }
 }
@@ -51,4 +68,5 @@ const styles = {
 
 mount(<Demo />, 'body');
 
-export default Demo;
+
+export default Demo
