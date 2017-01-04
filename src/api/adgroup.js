180f8d2b88332a8f8b2485f69e911bba9aeadb28
why <hongying.wangh@xibao100.com>
2017-01-04 11:15:29 +0800

    api

--- src/api/adgroup.js
+++ src/api/adgroup.js
@@ -2,7 +2,106 @@
 
 import QN from 'QAP-SDK';
 import * as DateAPi from './date';
+import { Tabbar, Button, Icon, ListView, Iconfont, Modal } from 'nuke';
 
+/*
+* 需要先将所有在售的宝贝取出来 因为已经推广的宝贝要显示在前面
+*/
+
+export function getOnsaleItem (page){
+    var page_no = page > 1 ? page : 1;
+    var page_size = 200;
+    QN.top.invoke({
+        query: {
+            method:'taobao.items.onsale.get',
+            fields:'pic_url,price,num_iid,title,num,cid',
+            page_no:page_no,
+            page_size:page_size
+        }
+    }).then((result)=>{
+            
+            var dataNum = parseInt(result.items_onsale_get_response.total_results);
+           
+            if(dataNum ===0 && page_no ===1){
+               return; 
+            } 
+            if(dataNum > 0){
+                var response = result.items_onsale_get_response.items.item;
+                localstore('onLineItem',response).then((result)=>{
+                     Modal.alert(JSON.stringify(result)); 
+                });
+                let getData = getLocalstore('onLineItem');
+                   Modal.alert(JSON.stringify(res));
+ /*
+                if(res){
+                    page_no = page_no+1;
+                    let totalpage = Math.ceil(dataNum / page_size);
+
+                    if(totalpage >= page_no)
+                    {
+                        setTimeout(function(){
+                            getOnsaleItem(title_seach,page_no,callback);
+                        },100);
+                    }else
+                    {
+                       return;
+                    }
+                }*/
+            }
+    })
+    .catch(error=>{
+        Modal.toast(error);
+    });  
+}
+function localstore(key,val){
+    return QN.database.execSql({
+      query: {
+        sql: 'CREATE TABLE IF NOT EXISTS OnlineItems(id integer PRIMARY KEY AUTOINCREMENT,num_iid varchar,num int, title varchar,price float,pic_url varchar,cid,varchar,img_url varchar,props varchar)',
+      },
+      success(result) {
+        if(result.result == 'success'){
+            for(let i=0;i<val.length;i++){
+                QN.database.execSql({
+                  query: {
+                    sql: ['INSERT INTO OnlineItems (num_iid,num,title,price,pic_url,cid,img_url,props) VALUES (?,?,?,?,?,?,?,?)', [val[i].num_iid, val[i].num, val[i].title,val[i].price,val[i].pic_url, val[i].cid,val[i].img_url,val[i].props]],
+                  },
+                  success(result) {
+                    
+                  },
+                  error(error) {
+                   
+                  },
+                });
+            }     
+        }
+      },
+      error(error) {
+        console.log('[ERROR]', error);
+      }
+    }).then((result)=>{
+        return result.result;
+    });
+}
+
+function getLocalstore(key){
+    QN.database.execSql({
+      query: {
+        sql: ['select * from OnlineItems limit 1'],
+      },
+      success(result) {
+    
+      },
+      error(error) {
+        console.log('[ERROR]', error);
+      },
+    }).then((result) => {
+      console.log('[RESOLVE]', result);
+      
+    }, (error) => {
+      console.log('[REJECT]', error);
+    });
+
+}
 export function getOnsaleItems (page){
     var page_no = page > 1 ? page : 1;
     var page_size = 200;
@@ -28,6 +127,8 @@ export function getOnsaleItems (page){
             Modal.toast(error);
         });  
 }
+
+
 export function getAdgroupsByCid(subway_token,campaign_id,page_no){
     var page = page_no > 0 ? page_no : 1,
         page_size = 200;
--- src/api/authsign.js
+++ src/api/authsign.js
@@ -1,18 +1,62 @@
 'use strict';
 import QN from 'QAP-SDK';
 import APIError from './checkerror';
+import { Tabbar, Button, Icon, ListView, Iconfont, Modal } from 'nuke';
+var key_name = 'subway_token';
 
+// 还需要检查一下token过期时间
 export function getAuthSign(){
-    return QN.top.invoke({
+
+    return  QN.sessionstore.get({
+        query: {
+            key: 'subway_token'
+        },
+        success(result) {
+                return result;
+        },
+        error(error) {
+          
+        }
+    }).then((result)=>{
+ 
+        if(result.data && result.data.subway_token){
+            return result.data.subway_token;
+        }else{
+               return getSign().then((result)=>{ //获取subway_token
+                    storeSubwayToken(result); // 存储stoken
+                    return result;
+                });  
+             }      
+    });
+}
+function getSign(){
+    return  QN.top.invoke({
             query: {
                  method:'taobao.simba.login.authsign.get'
             }
         }).then((result)=>{
-            return   result.simba_login_authsign_get_response.subway_token;
+            return result.simba_login_authsign_get_response.subway_token;
         })
         .catch(error => {
-            Modal.toast(APIError(error));
-        });  
+           return false;
+        });
+}
+function storeSubwayToken(val){
+    QN.sessionstore.set({
+        query: {
+                subway_token: val
+            },
+        success(result) {
+          
+        },
+        error(error) {
+           
+            }
+    }).then(result => {
+       
+    }, error => {
+        
+    });
 }
 
 
